cc5d3dffc09ed3142f48b8007832bb31bcfb5e22
maulik1110 <maulik.monkenterprise@gmail.com>
2024-04-14 01:49:10 +0530

    nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Body from './compinent/Body'
 
 const App = () => {
   return (
-    <div className='bg-orange-500'>
-      <h1>ncjnjm</h1>
-      <h5>cjm dokcm</h5>
+    <div >
+      <Body/>
     </div>
   )
 }
---
+++ src/compinent/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Body = () => {
+  return (
+    <div className='w-screen h-screen bg-black'>
+        <Nav/>
+    </div>
+  )
+}
+
+export default Body
