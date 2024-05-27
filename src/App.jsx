9bfb2604bd95c5a034f3fcab50e77878cf74e41b
maulik1110 <maulik.monkenterprise@gmail.com>
2024-05-28 00:32:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Body from './compinent/Body'
+// import LocomotiveScroll from 'locomotive-scroll';
+
+
 
 const App = () => {
+// const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='overflow-hidden overflow-y-auto'>
       <Body/>
